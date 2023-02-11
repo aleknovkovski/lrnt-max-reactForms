c09f54d1e06c56b1369acb0fe145e72e656991c1
Alek Novkovski <alek@greatcode2.com>
2023-02-11 23:22:11 +0100

    Complete lesson code; Shows getting values w both refs, 2-way-binding

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,42 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+import { useRef, useState } from 'react';
+
+function SimpleInput (props) {
+    const nameInputRef = useRef();
+    const [enteredName, setEnteredName] = useState('');
+
+    function nameInputChangeHandler (event) {
+        setEnteredName(event.target.value);
+    };
+
+    function formSubmissionHandler (event) {
+        event.preventDefault();
+
+        console.log(enteredName);
+
+        const enteredValue = nameInputRef.current.value;
+        console.log(enteredValue);
+
+        // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
+        setEnteredName('');
+    };
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            <div className='form-control'>
+                <label htmlFor='name'>Your Name</label>
+                <input
+                    ref={nameInputRef}
+                    type='text'
+                    id='name'
+                    onChange={nameInputChangeHandler}
+                    value={enteredName}
+                />
+            </div>
+            <div className='form-actions'>
+                <button>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
